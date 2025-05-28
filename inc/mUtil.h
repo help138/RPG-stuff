16ffb538ffba84ea97ba4d84a60910a305b8b199
help138 <Kmreece2006@gmail.com>
2025-05-28 13:51:12 -0400

    added charToNum function to mUtil.h

--- inc/mUtil.h
+++ inc/mUtil.h
@@ -41,6 +41,21 @@
     a = 97, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
   };
 
+int charToNum(char ch){
+  switch (ch){
+    case zero:    return 0;
+    case one:     return 1;
+    case two:     return 2;
+    case three:   return 3;
+    case four:    return 4;
+    case five:    return 5;
+    case six:     return 6;
+    case seven:   return 7;
+    case eight:   return 8;
+    case nine:    return 9;
+    default:      return (char)ch;
+  }
+}
   // TODO build the libary it self
 
   typedef struct {
